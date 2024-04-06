2a345e46a79ad9c4921242bdfb0a03b3046e9d44
Arvindithub <144795946+Arvindithub@users.noreply.github.com>
2024-04-06 16:13:00 +0530

    new commit

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,26 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+import { useUserContext } from "@/context/AuthContext";
+
+export default function AuthLayout() {
+  const { isAuthenticated } = useUserContext();
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img
+src='https://images.pexels.com/photos/1097456/pexels-photo-1097456.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'
+/>
+        </>
+      )}
+    </>
+  );
+}
